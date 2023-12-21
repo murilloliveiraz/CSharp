85db6f04695de089fb7350c9e6699c773cc77cb6
Murillo Oliveira <murillo1047@gmail.com>
2023-12-21 15:56:41 -0300

    feat: exercises

---
+++ readingDataExercise/readingDataExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace readingDataExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo:");
+            string fullName = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int bedrooms = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto:");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha, separado por espaco em branco' '):");
+            string[] vect = Console.ReadLine().Split(' ');
+            string lastName = vect[0];
+            int age = int.Parse(vect[1]);
+            double height = double.Parse(vect[2], CultureInfo.InvariantCulture);
+            Console.WriteLine(fullName);
+            Console.WriteLine(bedrooms);
+            Console.WriteLine(price.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(lastName);
+            Console.WriteLine(age);
+            Console.WriteLine(height.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ sequentialStructureExercises/sequentialStructureExercises/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sequentialStructureExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========== CALCULAdORA DE SOMA =============");
+            Console.WriteLine("escreva o numero 1");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("escreva o n2:");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("O resultado é "+ (n1+n2));
+
+            Console.WriteLine("========== CALXULAODRA DE AREA DO CIRCULO ==========");
+            Console.WriteLine("informe o valor da circunferencia do circulo:");
+            double circunference = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double pi = 3.14159;
+            double area = pi * circunference * circunference;
+            Console.WriteLine("a area do circulo é: " + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
