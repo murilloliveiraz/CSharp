eeb3418ecfaeaf8b583beeabd71545844f6153a9
Murillo Oliveira <murillo1047@gmail.com>
2023-12-21 11:50:13 -0300

    feat: variables onversion and casting

---
+++ ConversionAndCastingVariables/ConversionAndCastingVariables/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionAndCastingVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float x =4.5f;
+            double y = x;
+
+            Console.WriteLine(x);
+
+            double a;
+            float b;
+
+            a = 5.1;
+            b =  (float)a;
+            Console.WriteLine(b);
+
+            double c;
+            int d;
+
+            c = 5.1;
+            d = (int)c;
+            Console.WriteLine(d);
+
+            int n1 = 5;
+            int n2 = 2;
+
+            double result = (double)n1 / n2;
+
+            Console.WriteLine(result);
+        }
+    }
+}
