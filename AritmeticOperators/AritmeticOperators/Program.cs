716d0d89018bf98cda08a4b0d560256d2b6ec856
Murillo Oliveira <murillo1047@gmail.com>
2023-12-21 13:56:19 -0300

    feat: atirmetic operators

---
+++ AritmeticOperators/AritmeticOperators/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AritmeticOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 3 + 4 * 2;
+            int n2 = (3 + 4) * 2;
+            int n3 = 17 % 3;
+            double n4 = 10.0 / 8.0;
+            
+            double a = 1.0, b = -3.0, c = -4.0;
+
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+
+            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+        }
+    }
+}
