ff400cac6f1cfa2aea043a4fae3e240b5d947805
Murillo Oliveira <murillo1047@gmail.com>
2023-12-22 09:04:01 -0300

    feat: logic and comparative operators

---
+++ LogicOperators/LogicOperators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool c1 = 2 > 3 || 4 != 5; // true
+            bool c2 = !(2 > 3) && 4 != 5; // true
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine("--------------");
+            bool c3 = 10 < 5; // false
+            bool c4 = c1 || c2 && c3; // true
+            Console.WriteLine(c3);
+            Console.WriteLine(c4);
+        }
+    }
+}
