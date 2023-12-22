644fe35d491a76eef8da9f58d9c43858dde88c66
Murillo Oliveira <murillo1047@gmail.com>
2023-12-22 09:25:41 -0300

    feat: scope

---
+++ Scope/Scope/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scope
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine());
+            double desconto = 0.0;
+            if (preco > 100.0)
+            {
+                desconto = preco * 0.1;
+            }
+            Console.WriteLine(desconto);
+        }
+    }
+}
