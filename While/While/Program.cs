34878d1f1b09ffad9e0c005e90343207bd0e7563
Murillo Oliveira <murillo1047@gmail.com>
2023-12-23 19:57:17 -0300

    feat: while

---
+++ While/While/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero");
+            double x = double.Parse(Console.ReadLine());
+
+            while (x > 0)
+            {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Digite um outro numero");
+                x = double.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
