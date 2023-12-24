444f8aba323f04b04c2ad594781998853d7250a9
Murillo Oliveira <murillo1047@gmail.com>
2023-12-23 21:42:00 -0300

    feat: for

---
+++ For/For/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos números inteiros você vai digitar? ");
+            int N = int.Parse(Console.ReadLine());
+            int soma = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                Console.Write("Valor #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+            Console.WriteLine("Soma = " + soma);
+        }
+    }
+}
