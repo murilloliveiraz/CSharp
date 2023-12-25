79a41efeaf66adaaddab6e1cfd1dfa39c09392af
Murillo Oliveira <murillo1047@gmail.com>
2023-12-25 19:09:06 -0300

    feat: Methods

--- triangleAreaNoPOO/triangleAreaNoPOO/Program.cs
+++ triangleAreaNoPOO/triangleAreaNoPOO/Program.cs
@@ -14,7 +14,6 @@ namespace triangleAreaNoPOO
             Triangle x, y;
             x = new Triangle();
             y = new Triangle();
-
             Console.WriteLine("Entre com as medidas do triângulo X:");
             x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
@@ -23,10 +22,8 @@ namespace triangleAreaNoPOO
             y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double p = (x.A + x.B + x.C) / 2.0;
-            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
-            p = (y.A + y.B + y.C) / 2.0;
-            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+            double areaX = x.Area();
+            double areaY = y.Area();
             Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
             if (areaX > areaY)
--- triangleAreaNoPOO/triangleAreaNoPOO/Triangle.cs
+++ triangleAreaNoPOO/triangleAreaNoPOO/Triangle.cs
@@ -1,9 +1,17 @@
-﻿namespace triangleAreaNoPOO
+﻿using System;
+
+namespace triangleAreaNoPOO
 {
     internal class Triangle
     {
         public double A;
         public double B;
         public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
     }
 }
