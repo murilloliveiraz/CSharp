8221941d90cf0c5c5d80a6867262310547b24652
Murillo Oliveira <murillo1047@gmail.com>
2023-12-26 14:05:43 -0300

    feat: this

---
+++ This/PooProduct/Product.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+namespace PooProduct
+{
+    internal class Product
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Product()
+        {
+            Quantidade = 10;
+        }
+        public Product(string nome, double preco) : this()
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public Product(string nome, double preco, int quantidade) : this(nome, preco)
+        {
+            Quantidade = quantidade;
+        }
+
+
+        public double totalValueInStock()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void addValueInStock(int qte)
+        {
+            Quantidade += qte;
+        }
+
+        public void removeValueInStock(int qte)
+        {
+            Quantidade -= qte;
+        }
+
+        public override string ToString()
+        {
+            return Nome 
+                + ", $" 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture) 
+                + ", " 
+                + Quantidade 
+                + " unidades, Total $" 
+                + totalValueInStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
