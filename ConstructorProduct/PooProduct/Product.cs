2d6031680c7dd772734f7d5ca615aa67af0a7333
Murillo Oliveira <murillo1047@gmail.com>
2023-12-26 13:42:41 -0300

    feat: constructor

---
+++ ConstructorProduct/PooProduct/Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace PooProduct
+{
+    internal class Product
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Product(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public double totalValueInStock()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void addValueInStock(int qte)
+        {
+            Quantidade += qte;
+        }
+
+        public void removeValueInStock(int qte)
+        {
+            Quantidade -= qte;
+        }
+
+        public override string ToString()
+        {
+            return Nome 
+                + ", $" 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture) 
+                + ", " 
+                + Quantidade 
+                + " unidades, Total $" 
+                + totalValueInStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ConstructorProduct/PooProduct/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace PooProduct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.WriteLine("Nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Preço :");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantidade: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Product p = new Product(nome, preco, quantidade);
+
+            Console.WriteLine("Dados do produto "+ p);
+
+            Console.WriteLine();
+            Console.WriteLine("digite o numero de produtos a ser adicionado");
+            int qte = int.Parse(Console.ReadLine());
+            p.addValueInStock(qte);
+
+            Console.WriteLine("Dados do produto atualizados "+ p);
+
+            Console.WriteLine();
+            Console.WriteLine("digite o numero de produtos a ser removido");
+            qte = int.Parse(Console.ReadLine());
+            p.removeValueInStock(qte);
+
+            Console.WriteLine("Dados do produto atualizados "+ p);
+
+        }
+    }
+}
