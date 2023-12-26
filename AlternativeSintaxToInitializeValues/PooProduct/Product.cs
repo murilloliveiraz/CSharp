947a631152f6f3d1107a96b5f4b99829efc8317e
Murillo Oliveira <murillo1047@gmail.com>
2023-12-26 13:59:42 -0300

    feat: initializing class

---
+++ AlternativeSintaxToInitializeValues/PooProduct/Product.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+namespace PooProduct
+{
+    internal class Product
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Product(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        public Product(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 0;
+        }
+
+        public Product() { }
+
+        public double totalValueInStock()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void addValueInStock(int qte)
+        {
+            Quantidade += qte;
+        }
+
+        public void removeValueInStock(int qte)
+        {
+            Quantidade -= qte;
+        }
+
+        public override string ToString()
+        {
+            return Nome 
+                + ", $" 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture) 
+                + ", " 
+                + Quantidade 
+                + " unidades, Total $" 
+                + totalValueInStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
