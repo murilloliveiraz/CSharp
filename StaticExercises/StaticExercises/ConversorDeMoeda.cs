269bf4a243e31d028cf62725238a05dd02a6cba1
Murillo Oliveira <murillo1047@gmail.com>
2023-12-26 11:47:29 -0300

    feat: static exercise

---
+++ StaticExercises/StaticExercises/ConversorDeMoeda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercises
+{
+    internal class ConversorDeMoeda
+    {
+        public static double iof = 6.00;
+
+        public static double value(double dolar, double cotacao)
+        {
+            return dolar * cotacao + (dolar * cotacao * iof / 100);
+        }
+    }
+}
---
+++ StaticExercises/StaticExercises/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace StaticExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cotacao, dolares;
+            Console.WriteLine("Qual é a cotação do dólar");
+            cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantos dólares voce vai comprar");
+            dolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("o valor a ser pago é de " + ConversorDeMoeda.value(dolares, cotacao));
+        }
+    }
+}
