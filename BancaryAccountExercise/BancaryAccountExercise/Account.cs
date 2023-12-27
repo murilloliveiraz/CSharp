5801fe116aed5493ce1ade5e346916f2549a570a
Murillo Oliveira <murillo1047@gmail.com>
2023-12-27 16:57:03 -0300

    feat: bancary account

---
+++ BancaryAccountExercise/BancaryAccountExercise/Account.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+
+namespace BancaryAccountExercise
+{
+    internal class Account
+    {
+        public string Name { get; set; }
+        public int AccountNumber { get; private set; }
+        public double Balance { get; private set; }
+
+        public Account(string nome, int accountNumber)
+        {
+            Name = nome;
+            AccountNumber = accountNumber;
+            Balance = 0;
+        }
+        public Account(string nome, int accountNumber, double initialDeposit) : this(nome, accountNumber)
+        {
+            addBalance(initialDeposit);
+        }
+
+        public string Nome
+        {
+            get { return Name; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    Name = value;
+                }
+            }
+        }
+
+        public void addBalance(double value)
+        {
+            Balance += value;
+        }
+        public void withdrawBalance(double value)
+        {
+            double tax = 5.00;
+            Balance -= value + tax;
+        }
+
+        public override string ToString()
+        {
+            return "Numero da Conta : " 
+                + AccountNumber 
+                + "\n Nome :"
+                + Name 
+                + "\n Saldo: $" 
+                + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ BancaryAccountExercise/BancaryAccountExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace BancaryAccountExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account;
+            Console.WriteLine("========= BANCO =========");
+            Console.WriteLine("Entre o número da conta: ");
+            int accountNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre o titular da conta: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Haverá depósito inicial (s/n)?");
+            char resp = char.Parse(Console.ReadLine());
+            if(resp == 's' || resp == 'S')
+            {
+                Console.WriteLine("Entre o valor de depósito inicial:");
+                double initialValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account = new Account(name, accountNumber, initialValue);
+                Console.WriteLine(account);
+            } else
+            {
+                account = new Account(name, accountNumber);
+                Console.WriteLine(account);
+            }
+            Console.WriteLine("Entre um valor para depósito: ");
+            double deposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.addBalance(deposit);
+            Console.WriteLine("Dados atualizados da conta: "+ account);
+
+            Console.WriteLine("Entre um valor para saque: ");
+            double withDraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.withdrawBalance(withDraw);
+            Console.WriteLine("Dados atualizados da conta: " + account);
+        }
+    }
+}
