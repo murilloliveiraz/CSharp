77a374c8eb1fbb87d1476a0cecc4098f44490d83
Murillo Oliveira <murillo1047@gmail.com>
2023-12-28 10:16:34 -0300

    feat: calculator using ref e out

---
+++ RefAndOut/RefAndOut/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefAndOut
+{
+    internal class Calculator
+    {
+        public static void Triple(int origin, out int result)
+        {
+            result = origin * 3;
+        }
+    }
+}
---
+++ RefAndOut/RefAndOut/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefAndOut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+            Console.WriteLine(a);
+        }
+    }
+}
