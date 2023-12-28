5b84d8bc1e123736728b56cd61c43e210607e8e8
Murillo Oliveira <murillo1047@gmail.com>
2023-12-28 10:29:12 -0300

    feat: foreach

---
+++ ForEach/ForEach/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vect = new string[] { "Maria", "Bob", "Alex" };
+            foreach (string obj in vect)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
