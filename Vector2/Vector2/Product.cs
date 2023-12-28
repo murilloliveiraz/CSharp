fafc17f341ee59ede4b44aa9f9290dc309414af4
Murillo Oliveira <murillo1047@gmail.com>
2023-12-28 08:24:41 -0300

    feat: vectors part2

---
+++ Vector2/Vector2/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vector2
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
