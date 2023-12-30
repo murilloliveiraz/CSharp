54c320328e0e9e9bceaaf5f7108adb203ecc695a
Murillo Oliveira <murillo1047@gmail.com>
2023-12-30 19:30:36 -0300

    feat: date time functions

---
+++ DateTimeFunctions/DateTimeFunctions/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d = new DateTime(2001, 8, 15, 13, 45, 58, 275);
+            Console.WriteLine(d);
+            Console.WriteLine("1) Date: " + d.Date);
+            Console.WriteLine("2) Day: " + d.Day);
+            Console.WriteLine("3) DayOfWeek: " + d.DayOfWeek);
+            Console.WriteLine("4) DayOfYear: " + d.DayOfYear);
+            Console.WriteLine("5) Hour: " + d.Hour);
+            Console.WriteLine("6) Kind: " + d.Kind);
+            Console.WriteLine("7) Millisecond: " + d.Millisecond);
+            Console.WriteLine("8) Minute: " + d.Minute);
+            Console.WriteLine("9) Month: " + d.Month);
+            Console.WriteLine("10) Second: " + d.Second);
+            Console.WriteLine("11) Ticks: " + d.Ticks);
+            Console.WriteLine("12) TimeOfDay: " + d.TimeOfDay);
+            Console.WriteLine("13) Year: " + d.Year);
+
+            Console.WriteLine();
+            DateTime d1 = new DateTime(2001, 8, 15, 13, 45, 58);
+            string s1 = d1.ToLongDateString();
+            string s2 = d1.ToLongTimeString();
+            string s3 = d1.ToShortDateString();
+            string s4 = d1.ToShortTimeString();
+            string s5 = d1.ToString();
+            string s6 = d1.ToString("yyyy-MM-dd HH:mm:ss");
+            string s7 = d1.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+            Console.WriteLine(s5);
+            Console.WriteLine(s6);
+            Console.WriteLine(s7);
+        }
+    }
+}
