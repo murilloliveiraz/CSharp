237f6c9fdc1fc4c143fa71384613186b38b164ee
Murillo Oliveira <murillo1047@gmail.com>
2023-12-30 18:12:35 -0300

    feat: ternary condition

---
+++ ternaryCondition/ternaryCondition/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ternaryCondition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = 34.5;
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+            Console.WriteLine(preco - desconto);
+        }
+    }
+}
