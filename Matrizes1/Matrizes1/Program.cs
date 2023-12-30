f4575b612997652eaa7616cae4442f9c5501b09d
Murillo Oliveira <murillo1047@gmail.com>
2023-12-29 22:06:07 -0300

    feat: matriz exercise

---
+++ Matrizes1/Matrizes1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrizes1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] mat = new double[2, 3];
+            Console.WriteLine(mat.Length);
+            Console.WriteLine(mat.Rank);
+            Console.WriteLine(mat.GetLength(0));
+            Console.WriteLine(mat.GetLength(1));
+        }
+    }
+}
---
+++ Matrizexercise2/Matrizexercise2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Matrizexercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lenght = Console.ReadLine().Split(' ');
+            int m = int.Parse(lenght[0]);
+            int n = int.Parse(lenght[1]);
+
+            int[,] mat = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++) {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.WriteLine();
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                   if (mat[i, j] == number)
+                    {
+                        Console.WriteLine($"Position {i},{j}");
+                        if(j > 0)
+                        {
+                        Console.WriteLine($"Left {mat[i, j - 1]}");
+                        }
+                        if(j < n - 1)
+                        {
+                        Console.WriteLine($"Right {mat[i, j + 1]}");
+                        }
+                        if (i < n - 1)
+                        {
+                        Console.WriteLine($"Down {mat[i + 1 , j]}");
+                        }
+                        if(i > 0) 
+                        {
+                        Console.WriteLine($"Top {mat[i - 1 , j]}");
+                        }
+                    } 
+                }
+            }
+        }
+    }
+}
