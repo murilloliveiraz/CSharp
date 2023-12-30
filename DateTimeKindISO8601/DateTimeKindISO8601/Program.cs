5d3579ab68de65f0ea5e25c6f7ebd0d8e8008e4c
Murillo Oliveira <murillo1047@gmail.com>
2023-12-30 20:12:08 -0300

    feat: iso8601

---
+++ DateTimeKindISO8601/DateTimeKindISO8601/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeKindISO8601
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
+            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
+            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
+            Console.WriteLine("d1: " + d1);
+            Console.WriteLine("d1 Kind: " + d1.Kind);
+            Console.WriteLine("d1 to Local: " + d1.ToLocalTime());
+            Console.WriteLine("d1 to Utc: " + d1.ToUniversalTime());
+            Console.WriteLine();
+            Console.WriteLine("d2: " + d2);
+            Console.WriteLine("d2 Kind: " + d2.Kind);
+            Console.WriteLine("d2 to Local: " + d2.ToLocalTime());
+            Console.WriteLine("d2 to Utc: " + d2.ToUniversalTime());
+            Console.WriteLine();
+            Console.WriteLine("d3: " + d3);
+            Console.WriteLine("d3 Kind: " + d3.Kind);
+            Console.WriteLine("d3 to Local: " + d3.ToLocalTime());
+            Console.WriteLine("d3 to Utc: " + d3.ToUniversalTime());
+
+            DateTime d3 = DateTime.Parse("2000-08-15 13:05:58");
+            DateTime d4 = DateTime.Parse("2000-08-15T13:05:58Z"); // cria local DateTime
+            Console.WriteLine("d1: " + d3);
+            Console.WriteLine("d1 Kind: " + d3.Kind);
+            Console.WriteLine("d1 to Local: " + d3.ToLocalTime());
+            Console.WriteLine("d1 to Utc: " + d3.ToUniversalTime());
+            Console.WriteLine();
+            Console.WriteLine("d2: " + d4);
+            Console.WriteLine("d2 Kind: " + d4.Kind);
+            Console.WriteLine("d2 to Local: " + d4.ToLocalTime());
+            Console.WriteLine("d2 to Utc: " + d4.ToUniversalTime());
+            Console.WriteLine();
+            Console.WriteLine(d4.ToString("yyyy-MM-ddTHH:mm:ssZ")); // cuidado!
+            Console.WriteLine(d4.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"));
+        }
+    }
+}
