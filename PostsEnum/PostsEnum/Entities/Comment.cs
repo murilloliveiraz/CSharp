66e73bd0b7e12ba9e522850854f2a7b0dcc35f61
Murillo Oliveira <murillo1047@gmail.com>
2024-01-08 10:38:55 -0300

    feat: social media enumerator exercise

---
+++ PostsEnum/PostsEnum/Entities/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostsEnum.Entities
+{
+    internal class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
