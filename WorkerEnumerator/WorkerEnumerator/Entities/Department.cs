92198251b0afb88c5ea87a7cfc87d42af10ecee3
Murillo Oliveira <murillo1047@gmail.com>
2024-01-08 10:03:10 -0300

    feat: worker enumerator exercise

---
+++ WorkerEnumerator/WorkerEnumerator/Entities/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkerEnumerator.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+            
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ WorkerEnumerator/WorkerEnumerator/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkerEnumerator.Enums
+{
+    internal enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
