1976e006c4419190bde968579fd1ed226564bc46
Murillo Oliveira <murillo1047@gmail.com>
2024-01-09 09:37:28 -0300

    feat: upcasting and downcasting

---
+++ UpcastingAndDowncasting/Inheritance/Entities/SavingsAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UpcastingAndDowncasting.Entities;
+
+namespace Inheritance.Entities
+{
+    internal class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
