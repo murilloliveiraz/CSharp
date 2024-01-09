b2275d337302030c2d2ed6e18d32905e9a9e683a
Murillo Oliveira <murillo1047@gmail.com>
2024-01-09 08:37:57 -0300

    feat: inheritance

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using Inheritance.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
