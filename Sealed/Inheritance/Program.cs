939546f72b366ec4e99c6592a70d345624ac4dd2
Murillo Oliveira <murillo1047@gmail.com>
2024-01-09 11:51:29 -0300

    feat: sealed

---
+++ Sealed/Inheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿using UpcastingAndDowncasting.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inheritance.Entities;
+
+namespace UpcastingAndDowncasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(1001, "Alex", 500.0);
+            Account acc2 = new SavingsAccount(1001, "Alex", 500.0, 0.01);
+
+
+            acc1.WithDraw(10.0);
+            acc2.WithDraw(10.0);
+
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
+        }
+    }
+}
