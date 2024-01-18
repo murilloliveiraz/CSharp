303ab59a05b0f44a2185541ae2751b73d95e424c
Murillo Oliveira <murillo1047@gmail.com>
2024-01-18 11:49:01 -0300

    feat: file and fileinfo

---
+++ FileInfo/FileInfo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\david.soares\OneDrive - Cobreflex\Área de Trabalho\file1.txt";
+            string targetPath = @"C:\Users\david.soares\OneDrive - Cobreflex\Área de Trabalho\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e) 
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
