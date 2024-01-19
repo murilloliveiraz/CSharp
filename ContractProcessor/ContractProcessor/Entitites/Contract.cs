7c3d98ed052263d0a95e0d2f0cefb36b92d5e4b9
Murillo Oliveira <murillo1047@gmail.com>
2024-01-19 14:34:45 -0300

    feat: Contract installment interfaces

---
+++ ContractProcessor/ContractProcessor/Entitites/Contract.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContractProcessor.Entitites
+{
+    internal class Contract
+    {
+        public int number { get; private set; }
+        public  DateTime date { get; private set; }
+        public double totalValue { get; private set; }
+        public List<Installment> installments = new List<Installment>();
+
+        public Contract(int number, DateTime date, double totalValue)
+        {
+            this.number = number;
+            this.date = date;
+            this.totalValue = totalValue;
+        }
+
+        public void AddInstallment(Installment installment)
+        {
+            installments.Add(installment);
+        }
+
+        public override string ToString()
+        {
+            System.Text.StringBuilder result = new System.Text.StringBuilder();
+            foreach (Installment installment in installments)
+            {
+                result.AppendLine(installment.ToString());
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ ContractProcessor/ContractProcessor/Services/ContractService.cs
@@ -0,0 +1,29 @@
+﻿using ContractProcessor.Entitites;
+using System;
+
+namespace ContractProcessor.Services
+{
+    internal class ContractService
+    {
+        private IOnlinePaymentService _onlinePaymentService;
+
+        public ContractService(IOnlinePaymentService paypal)
+        {
+            _onlinePaymentService = paypal;
+        }
+
+        public void ProcessContract(Contract contract, int Months)
+        {
+            double monthlyInstallmentWithOutTaxes = contract.totalValue / Months;
+            for(int i = 1; i <= Months; i++)
+            {
+                DateTime dateTime = contract.date.AddMonths(i);
+                double paymentTax = monthlyInstallmentWithOutTaxes + _onlinePaymentService.PaymentFee(monthlyInstallmentWithOutTaxes);
+                double InterestTax = _onlinePaymentService.Interest(paymentTax, i);
+                double installmentValue = paymentTax + InterestTax;
+                Installment installment = new Installment(dateTime, installmentValue);
+                contract.AddInstallment(installment);
+            }
+        }
+    }
+}
