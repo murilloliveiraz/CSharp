ebb272dd0be47789e1c4e96d2580564a2643ff6d
Murillo Oliveira <murillo1047@gmail.com>
2024-01-20 21:02:40 -0300

    feat: generics introduction

---
+++ Generics/Generics/Entities/PrintService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.Entities
+{
+    internal class PrintService<T>
+    {
+
+        private T[] _values = new T[10];
+        private int _count = 0;
+
+        public void AddValue(T value)
+        {
+            if(_count == 10)
+            {
+                throw new InvalidOperationException("PrintService is already full!");
+
+            }
+            _values[_count] = value;
+            _count++;
+        }
+
+        public T First()
+        {
+            if (_count == 0) 
+            {
+                throw new InvalidOperationException("PrintService is empty!");
+            }
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for(int i= 0; i < _count - 1; i++)
+            {
+                Console.Write($"{_values[i]}, ");
+            }
+            Console.WriteLine($"{_values[_count - 1]} ]");
+        }
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿using Generics.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<int> printService = new PrintService<int>();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: "+ printService.First());
+        }
+    }
+}
