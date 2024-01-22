8a274f469e6bf53801d850fcd219daa2422be6e1
Murillo Oliveira <murillo1047@gmail.com>
2024-01-22 08:04:02 -0300

    feat: equals and hash code

---
+++ EqualsAndHashCode/EqualsAndHashCode/Entities/CLient.cs
@@ -0,0 +1,20 @@
+﻿namespace EqualsAndHashCode.Entities
+{
+    internal class CLient
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is CLient)) return false;
+            CLient other = obj as CLient;
+            return Email.Equals(other.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ EqualsAndHashCode/EqualsAndHashCode/Program.cs
@@ -0,0 +1,23 @@
+﻿using EqualsAndHashCode.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualsAndHashCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CLient a = new CLient { Name = "Maria", Email = "maria@gmail.com"};
+            CLient b = new CLient { Name = "Alex", Email = "alex@gmail.com"};
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
