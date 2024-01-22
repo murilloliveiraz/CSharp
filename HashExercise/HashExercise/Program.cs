9cc727ba6947cc8716c46ab6f3902d99e0bf9aa2
Murillo Oliveira <murillo1047@gmail.com>
2024-01-22 11:19:01 -0300

    feat: hash exercise

---
+++ HashExercise/HashExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using HashExercise.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HashExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { Username = name, Instant = instant });
+                    }
+                }
+                Console.WriteLine("total users: "+ set.Count);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
