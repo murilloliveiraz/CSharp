1bc089101d407088759ab84365f365fc162c7962
Murillo Oliveira <murillo1047@gmail.com>
2024-01-22 15:35:51 -0300

    feat: dictionary exercise

---
+++ DictionaryExercise/DictionaryExercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> set = new Dictionary<string, int>();
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int votes = int.Parse(line[1]);
+                        set[name] = set.ContainsKey(name) ? set[name] += votes : set[name] = votes;
+                    }
+                }
+                foreach(KeyValuePair<string, int> pair in set) 
+                {
+                    Console.WriteLine(pair.Key + ":" + pair.Value);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
