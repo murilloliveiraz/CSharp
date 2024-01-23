a38a9227ef482465295e1cb7770e58d8fdf9cd17
Murillo Oliveira <murillo1047@gmail.com>
2024-01-23 15:29:15 -0300

    feat: multicast delegates

---
+++ MulticastDelegates/MulticastDelegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using MulticastDelegates.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastDelegates
+{
+    internal class Program
+    {
+        delegate void BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+            op(a, b);
+        }
+    }
+}
