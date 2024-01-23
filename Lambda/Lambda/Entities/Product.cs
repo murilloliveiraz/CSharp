9c345705143483d874687901b6777fa1d529f95c
Murillo Oliveira <murillo1047@gmail.com>
2024-01-23 14:51:05 -0300

    feat: lambda

---
+++ Lambda/Lambda/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Lambda.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
