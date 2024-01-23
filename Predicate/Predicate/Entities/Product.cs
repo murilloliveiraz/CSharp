098112536b88818c4af735a1ae5a058946e3b902
Murillo Oliveira <murillo1047@gmail.com>
2024-01-23 15:56:27 -0300

    feat: predicate

---
+++ Predicate/Predicate/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Predicate.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2");
+        }
+    }
+}
