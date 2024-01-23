4004e7c744f24c10c2a317b0a33d2de7beaf97dd
Murillo Oliveira <murillo1047@gmail.com>
2024-01-23 15:15:07 -0300

    feat: delegates

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using Delegates.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        delegate double BinaryNumericOperation(double n1, double n2);
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            // BinaryNumericOperation op = CalculationService.Sum;
+            BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+            // double result = op(a, b);
+            double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+
+        }
+    }
+}
