7b72f3b170ed074e9523eddf6f66c009b65a3ff8
Murillo Oliveira <murillo1047@gmail.com>
2024-01-24 16:05:26 -0300

    feat: linq

---
+++ FuncWithSelect/FuncWithSelect/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FuncWithSelect.Entities;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncWithSelect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            List<string> result = list.Select(p => p.Name.ToUpper()).ToList();
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ Linq/Linq/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Linq.Entities
+{
+    internal class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
