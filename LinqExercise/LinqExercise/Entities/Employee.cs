03ad3401c5ca1835d87ba233f40d84004acac7f1
Murillo Oliveira <murillo1047@gmail.com>
2024-01-26 14:14:24 -0300

    feat: linq exercise

---
+++ LinqExercise/LinqExercise/Entities/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExercise.Entities
+{
+    internal class Employee
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public double salary { get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            this.name = name;
+            this.email = email;
+            this.salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{name}, {email}, {salary.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Employee)) return false;
+            Employee other = obj as Employee;
+            return email.Equals(other.email);
+        }
+
+        public override int GetHashCode()
+        {
+            return email.GetHashCode();
+        }
+    }
+}
---
+++ LinqExercise/LinqExercise/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.IO;
+using LinqExercise.Entities;
+using System.Linq;
+
+namespace LinqExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Employee> set = new HashSet<Employee>();
+            Console.WriteLine("Enter the file full path:");
+            string path = Console.ReadLine();
+            Console.WriteLine("Enter salary: ");
+            double consultSalary = double.Parse(Console.ReadLine());
+            try
+            {
+                using(StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        string email = line[1];
+                        double salary = double.Parse(line[2], CultureInfo.InvariantCulture);
+                        set.Add(new Employee(name, email, salary));
+                    }
+                }
+                var emails = set.Where(emp => emp.salary >= consultSalary).OrderBy(emp => emp.salary);
+                var sum = set.Where(emp => emp.name[0] == 'M').Sum(emp => emp.salary);
+                foreach (Employee emp in emails)
+                {
+                    Console.WriteLine(emp.email);
+                }
+                Console.WriteLine("Sum of salary of people whose name starts with 'M':" + sum);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
+
